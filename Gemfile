c03fdb92a719f3772f510f11aab4e076e5d64d7e
Richard Stock <rstock.co@gmail.com>
2020-09-22 17:24:39 -0600

    add variable titles and site navigation

--- Gemfile
+++ Gemfile
@@ -2,3 +2,11 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'rerun'
+
+group :test do
+  gem 'minitest'
+  gem 'minitest-reporters'
+  gem 'rack-test'
+  gem 'rake'
+  gem 'nokogiri'
+end
---
+++ test/site_pages_test.rb
@@ -0,0 +1,35 @@
+require_relative 'test_helper'
+
+class PalindromeAppTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_index
+    get '/'
+    assert last_response.ok?
+    assert doc(last_response).at_css('h1')
+    assert_equal "Learn Enough Ruby Sample App | Home",
+      doc(last_response).at_css('title').content
+    assert doc(last_response).at_css('nav')
+  end
+
+  def test_about
+    get '/about'
+    assert last_response.ok?
+    assert doc(last_response).at_css('h1')
+    assert_equal "Learn Enough Ruby Sample App | About",
+      doc(last_response).at_css('title').content
+
+  end
+
+  def test_palindrome
+    get '/palindrome'
+    assert last_response.ok?
+    assert doc(last_response).at_css('h1')
+    assert_equal "Learn Enough Ruby Sample App | Palindrome Detector",
+      doc(last_response).at_css('title').content
+  end
+end
